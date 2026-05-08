06e96da8d79d35a20cf8fcf3825bb87045c3ec9e
Nagasudhir <nagasudhirpulla@gmail.com>
2026-05-08 09:16:23 +0530

    started create user page

---
+++ App/Buildings/Queries/GetBuildings/GetBuildingsQuery.cs
@@ -0,0 +1,17 @@
+﻿using App.Common.Interfaces;
+using App.Common.Security;
+using Domain.Entities;
+
+namespace App.Buildings.Queries.GetBuildings;
+
+[Authorize]
+public record GetBuildingsQuery : IRequest<List<Building>>;
+
+public class GetBuildingsQueryHandler(IApplicationDbContext dbContext) : IRequestHandler<GetBuildingsQuery, List<Building>>
+{
+    public async Task<List<Building>> Handle(GetBuildingsQuery req, CancellationToken cancellationToken)
+    {
+        List<Building> buildings = await dbContext.Buildings.AsNoTracking().ToListAsync(cancellationToken);
+        return buildings;
+    }
+}
